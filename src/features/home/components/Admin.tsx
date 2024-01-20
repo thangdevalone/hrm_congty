fdca687ed37ae4eeec4c4bc4d1d1f6b5b670e909
haidaqn <haidang02032003@gmail.com>
2024-01-20 10:23:26 +0700

    dang

--- src/features/home/components/Admin.tsx
+++ src/features/home/components/Admin.tsx
@@ -10,6 +10,7 @@ import {
     navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const Admin = () => {
     return (
@@ -18,37 +19,38 @@ export const Admin = () => {
                 <Navbar />
                 <div className="pl-4 pr-[70px] relative z-10 pt-1 pb-4 flex justify-between flex-row">
                     <NavigationMenu>
-                        <NavigationMenuList> 
+                        <NavigationMenuList>
                             <NavigationMenuItem>
                                 <NavigationMenuLink
-                                    href="./accounts"
+                                    asChild
                                     className={navigationMenuTriggerStyle()}
                                 >
-                                    Tài khoản
+                                    <Link to="./accounts"> Tài khoản</Link>
                                 </NavigationMenuLink>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
                                 <NavigationMenuTrigger>Quản lý</NavigationMenuTrigger>
                                 <NavigationMenuContent>
                                     <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-2">
-                                        <ListItem href="./job" title="Quản lý chức vụ">
-                                            Thêm sửa xóa chức vụ trong công ty.
-                                        </ListItem>
-                                        <ListItem
-                                            href="./department"
-                                            title="Quản lý phòng ban"
-                                        >
-                                             Thêm sửa xóa phòng ban trong công ty.
-                                        </ListItem>
+                                        <Link to="./job">
+                                            <ListItem title="Quản lý chức vụ">
+                                                Thêm sửa xóa chức vụ trong công ty.
+                                            </ListItem>
+                                        </Link>
+                                        <Link to="./department">
+                                            <ListItem title="Quản lý phòng ban">
+                                                Thêm sửa xóa phòng ban trong công ty.
+                                            </ListItem>
+                                        </Link>
                                     </ul>
                                 </NavigationMenuContent>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
                                 <NavigationMenuLink
-                                    href="./organization"
+                                    asChild
                                     className={navigationMenuTriggerStyle()}
                                 >
-                                    Tổ chức
+                                    <Link to="./organization">Tổ chức</Link>
                                 </NavigationMenuLink>
                             </NavigationMenuItem>
                         </NavigationMenuList>
