d5ed5344886df2f0bfd8e126469bf7052ba2b608
ThangDevAlone <quangthangvtlg@gmail.com>
2024-03-05 15:44:46 +0700

    hehe

--- src/components/SidebarLeft.tsx
+++ src/components/SidebarLeft.tsx
@@ -2,16 +2,49 @@ import { useTheme } from '@/components/theme-provider';
 import { cn } from '@/lib/utils';
 import { PermissionProvider } from '@/utils';
 import { TextAlignJustifyIcon } from '@radix-ui/react-icons';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Icons } from './icons';
 import { Button } from './ui/button';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Lock, LogOut, Settings, UserRound } from 'lucide-react';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from './ui/alert-dialog';
+import { STATIC_HOST_NO_SPLASH } from '@/constants';
+import { authActions } from '@/features/auth/AuthSlice';
+import { useState } from 'react';
+import { useInfoUser } from '@/hooks';
+import { useDispatch } from 'react-redux';
 
 interface SidebarProps {
     collapse: boolean;
     className: string;
     setCollapse: (newCollapse: boolean) => void;
 }
+function shorten(username: string): string {
+    console.log(username)
+    const parts = username.trim().split(" ");
 
+    if (parts.length <= 2) {
+      return parts[0] + (parts[1] ? " " + parts[1] : ""); 
+    }
+  
+    return parts.slice(0, -1).map(part => part[0] + ".").join("").slice(0, -1) + " " + parts.pop();
+}
 export const SidebarLeft = ({ className, collapse, setCollapse }: SidebarProps) => {
     const navitage = useNavigate();
     const theme = useTheme();
@@ -21,99 +54,164 @@ export const SidebarLeft = ({ className, collapse, setCollapse }: SidebarProps) 
         navitage(`/home/${router}`);
     };
     const P = PermissionProvider();
-
+    const user = useInfoUser();
+    const dispatch = useDispatch();
+    const [openAlertLogout, setOpenAlertLogout] = useState(false);
+    const handleLogout = () => {
+        dispatch(authActions.logout());
+    };
     return (
-        <div className={cn('pb-12 dark:border-r side-bs', className)}>
-            <div className="space-y-4 py-4 ">
-                <div className="mb-4 px-5 gap-3 flex-row flex items-center">
-                    <Button size="icon" onClick={() => setCollapse(!collapse)}>
-                        <TextAlignJustifyIcon />
-                    </Button>
-                </div>
-                <div className="px-3 py-2">
-                    <div className="space-y-2">
-                        <Button
-                            onClick={() => handleNavitage('overview')}
-                            variant={location.pathname.includes('overview') ? 'secondary' : 'ghost'}
-                            className="w-full gap-3 justify-start h-10"
-                        >
-                            <Icons.overview
-                                className="w-5 h-5"
-                                color={theme.theme === 'dark' ? '#ffffff' : ''}
-                            />
-                            {!collapse && 'Tổng quan'}
+        <div className={cn('pb-5 dark:border-r side-bs', className)}>
+            <div className="flex flex-col h-full justify-between">
+                <div className="space-y-4 py-4 ">
+                    <div className="mb-4 px-5 gap-3 flex-row flex items-center">
+                        <Button size="icon" onClick={() => setCollapse(!collapse)}>
+                            <TextAlignJustifyIcon />
                         </Button>
-                        {P?.IS_ADMIN && (
+                    </div>
+                    <div className="px-3 py-2">
+                        <div className="space-y-2">
                             <Button
-                                onClick={() => handleNavitage('admin')}
+                                onClick={() => handleNavitage('overview')}
                                 variant={
-                                    location.pathname.includes('admin') ? 'secondary' : 'ghost'
+                                    location.pathname.includes('overview') ? 'secondary' : 'ghost'
                                 }
                                 className="w-full gap-3 justify-start h-10"
                             >
-                                <Icons.dashboard
+                                <Icons.overview
                                     className="w-5 h-5"
                                     color={theme.theme === 'dark' ? '#ffffff' : ''}
                                 />
-                                {!collapse && 'Trang quản trị'}
+                                {!collapse && 'Tổng quan'}
                             </Button>
-                        )}
-                        {P?.IS_ADMIN_OR_HR_MANAGER && (
+                            {P?.IS_ADMIN && (
+                                <Button
+                                    onClick={() => handleNavitage('admin')}
+                                    variant={
+                                        location.pathname.includes('admin') ? 'secondary' : 'ghost'
+                                    }
+                                    className="w-full gap-3 justify-start h-10"
+                                >
+                                    <Icons.dashboard
+                                        className="w-5 h-5"
+                                        color={theme.theme === 'dark' ? '#ffffff' : ''}
+                                    />
+                                    {!collapse && 'Trang quản trị'}
+                                </Button>
+                            )}
+                            {P?.IS_ADMIN_OR_HR_MANAGER && (
+                                <Button
+                                    onClick={() => handleNavitage('info-employee')}
+                                    variant={
+                                        location.pathname.includes('info-employee')
+                                            ? 'secondary'
+                                            : 'ghost'
+                                    }
+                                    className="w-full gap-3 justify-start h-10"
+                                >
+                                    <Icons.group
+                                        className="w-5 h-5"
+                                        color={theme.theme === 'dark' ? '#ffffff' : 'black'}
+                                    />
+                                    {!collapse && 'Nhân viên'}
+                                </Button>
+                            )}
                             <Button
-                                onClick={() => handleNavitage('info-employee')}
+                                onClick={() => handleNavitage('time-keep')}
                                 variant={
-                                    location.pathname.includes('info-employee')
-                                        ? 'secondary'
-                                        : 'ghost'
+                                    location.pathname.includes('time-keep') ? 'secondary' : 'ghost'
                                 }
                                 className="w-full gap-3 justify-start h-10"
                             >
-                                <Icons.group
+                                <Icons.leave
                                     className="w-5 h-5"
                                     color={theme.theme === 'dark' ? '#ffffff' : 'black'}
                                 />
-                                {!collapse && 'Nhân viên'}
+                                {!collapse && 'Chấm công'}
                             </Button>
-                        )}
-                        <Button
-                            onClick={() => handleNavitage('time-keep')}
-                            variant={
-                                location.pathname.includes('time-keep') ? 'secondary' : 'ghost'
-                            }
-                            className="w-full gap-3 justify-start h-10"
-                        >
-                            <Icons.leave
-                                className="w-5 h-5"
-                                color={theme.theme === 'dark' ? '#ffffff' : 'black'}
-                            />
-                            {!collapse && 'Chấm công'}
-                        </Button>
-                        <Button
-                            onClick={() => handleNavitage('schedules')}
-                            variant={
-                                location.pathname.includes('schedules') ? 'secondary' : 'ghost'
-                            }
-                            className="w-full gap-3 justify-start h-10"
-                        >
-                            <Icons.schedule
-                                className="w-5 h-5"
-                                color={theme.theme === 'dark' ? '#ffffff' : 'black'}
-                            />
-                            {!collapse && 'Đăng kí lịch'}
-                        </Button>
-                        <Button
-                            onClick={() => handleNavitage('leave')}
-                            variant={location.pathname.includes('leave') ? 'secondary' : 'ghost'}
-                            className="w-full gap-3 justify-start h-10"
-                        >
-                            <Icons.time
-                                className="w-5 h-5"
-                                color={theme.theme === 'dark' ? '#ffffff' : 'black'}
-                            />
-                            {!collapse && 'Nghỉ phép'}
-                        </Button>
+                            <Button
+                                onClick={() => handleNavitage('schedules')}
+                                variant={
+                                    location.pathname.includes('schedules') ? 'secondary' : 'ghost'
+                                }
+                                className="w-full gap-3 justify-start h-10"
+                            >
+                                <Icons.schedule
+                                    className="w-5 h-5"
+                                    color={theme.theme === 'dark' ? '#ffffff' : 'black'}
+                                />
+                                {!collapse && 'Đăng kí lịch'}
+                            </Button>
+                            <Button
+                                onClick={() => handleNavitage('leave')}
+                                variant={
+                                    location.pathname.includes('leave') ? 'secondary' : 'ghost'
+                                }
+                                className="w-full gap-3 justify-start h-10"
+                            >
+                                <Icons.time
+                                    className="w-5 h-5"
+                                    color={theme.theme === 'dark' ? '#ffffff' : 'black'}
+                                />
+                                {!collapse && 'Nghỉ phép'}
+                            </Button>
+                        </div>
                     </div>
                 </div>
+                <div className="px-5">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <div className='flex flex-row gap-2 items-center cursor-pointer'>
+                                <img
+                                    src={`${STATIC_HOST_NO_SPLASH + user?.PhotoPath}`}
+                                    alt="avatar"
+                                    className=" cursor-pointer w-10 h-10 border rounded-full"
+                                />
+                                {user && user.EmpName && <strong>{shorten(user.EmpName)}</strong>}{' '}
+                            </div>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent side="left" align="end">
+                            <DropdownMenuLabel>Tài khoản</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem asChild>
+                                <Link to="/settings/profile" className="cursor-pointer flex gap-2">
+                                    <UserRound /> Thông tin cá nhân
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem asChild>
+                                <Link to="/settings/password" className="cursor-pointer flex gap-2">
+                                    <Lock /> Quản lý mật khẩu
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem asChild>
+                                <Link to="/settings" className="cursor-pointer flex gap-2">
+                                    <Settings /> Cài đặt
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                                onClick={() => setOpenAlertLogout(true)}
+                                className="cursor-pointer flex gap-2"
+                            >
+                                <LogOut />
+                                Đăng xuất
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+                <AlertDialog open={openAlertLogout} onOpenChange={setOpenAlertLogout}>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Bạn có chắc muốn đăng xuất?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Đăng xuất xong bạn sẽ không thể thao tác với dữ liệu của ứng dụng
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Hủy</AlertDialogCancel>
+                            <AlertDialogAction onClick={handleLogout}>Đăng xuất</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
             </div>
         </div>
     );
